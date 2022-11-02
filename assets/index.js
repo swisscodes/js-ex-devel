ce4b77a17abe2406586b89814b2311f9d2ee6a16
Obafemi Lawal <swissbobo@gmail.com>
2022-11-02 16:21:40 +0300

    js-ex-develhope

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,18 @@
-const qsById = (id) => document.getElementById(id);
-const form = qsById('testform');
+const getElById = (id) => document.getElementById(id);
+const form = getElById('testform');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  console.log(form.elements['book_title'].value);
+  const bookTitle = form.elements['book_title'].value;
 
-  localStorage.setItem('book_title', JSON.stringify(bookTitle));
-  render(value);
+  localStorage.setItem('book title', JSON.stringify(bookTitle));
+  render();
 });
 
-function render(value) {
+function render() {
   const h3 = document.createElement('h3');
-  h3.innerText = localStorage.getItem(value);
-  const bookTitle = form.elements['book_title'].value;
-  document.appendChild(h3);
+  h3.innerText = localStorage.getItem('book title');
+  console.log(h3);
+  document.body.appendChild(h3);
 }
